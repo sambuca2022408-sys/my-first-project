b9e4d5fd194c6f6355bbdf6e4f1374157d31bc79
samir chand <sambuca2022408@gmail.com>
2026-09-02 10:55:52 +0545

    Fix uvicorn

--- agri_bot/main.py
+++ agri_bot/main.py
@@ -150,3 +150,9 @@ async def receive(request: Request):
 @app.get("/")
 def home():
     return {"status": "AgriBot Pro Money Machine Running 🌾💰"}
+    import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
